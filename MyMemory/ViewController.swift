0841577c64d533fffebde5b231f50eaff07e0f27
hwan Yu <hwanungyu@hwanui-MacBook-Pro.local>
2017-08-28 18:46:20 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by hwan ung Yu on 2017. 8. 28..
+//  Copyright © 2017년 hwan ung Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
